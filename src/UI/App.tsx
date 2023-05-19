fcf7a10fb3871fa999bc0a754b4c0765aebf2ca3
sZeuSz <rosenosilva@super.ufam.edu.br>
2023-05-19 13:11:42 -0400

    feat: project setup

---
+++ src/UI/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { GlobalStyled } from "styles";
+
+const App: React.FC = () => {
+  return (
+    <>
+      APP
+      <GlobalStyled />
+    </>
+  );
+};
+
+export default App;
